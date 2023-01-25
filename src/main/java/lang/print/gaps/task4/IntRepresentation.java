3101e70ca35248268f714236217d579f4fbcc1e9
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-25 18:04:30 +0300

    done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.task4;
 
 public class IntRepresentation {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //104, 97, 114
+
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.print(c);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(r);
     }
 }
